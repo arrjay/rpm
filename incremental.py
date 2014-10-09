fb89a224709ece966876b6a4b3428e29f583c10f
RJ Bergeron <rbergero@gmail.com>
2014-10-09 16:20:42 -0400

    shell combatible!

--- incremental.py
+++ incremental.py
@@ -250,14 +250,11 @@ except:
 # now sign the RPMs per output directory. note that check_call here uses shell globbing.
 # we don't sign debuginfo (today?)
 print "Signing built packages"
-rpmsign = ['./buildsystem/rpmsign.exp',
-    '--macros']
+rpmsign = './buildsystem/rpmsign.exp --macros']
 for mock_tuple in mockups:
     distdata = mock_tuple.split('-')
     reposub = mockrevmap[distdata[1]]
-    dist_sign = rpmsign
-    dist_sign.extend([base_rpmmacropath + rpmmacrodir + reposub, '--addsign',
-        reposub + '/' + distdata[2] + '/*.rpm'])
+    dist_sign = rpmsign + base_rpmmacropath + rpmmacrodir + reposub +' --addsign ' + reposub + '/' + distdata[2] + '/*.rpm'])
     try:
         subprocess.check_call(dist_sign, shell=True)
     except:
