0e4d7e2fa75ecfd30003b92a0b67ee63717e8a7a
RJ Bergeron <rbergero@gmail.com>
2014-11-06 12:14:44 -0500

    improve on the build code by providing a job that can run against branches. this won't run outside a git repo.

---
+++ buildsystem/contextual.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set +e
+
+# this script is not as flexible as intremental.py, but it allows you to build a branch for $DISTS on x86_64, which is 99% of what we care about.
+DISTS="el5 el6 el7"
+MOCKCONF="buildsystem/mock"
+
+# within the context of a tree, perform a build of only the files that have changed between master and that tree.
+me=$(git rev-parse --abbrev-ref HEAD)
+
+case $me in 
+  master)
+    echo "contextual branch build is not intended for master, that should be handled by incremental/stashbot publish"
+    exit 1
+    ;;
+esac
+
+rpmrepo="${HOME}/rpm-repos/inprogress/${me}"
+
+# get objects that are different between master and ourselves - since we should only be running against a commit, we're not worrying about untracked files.
+output=$(git diff --name-only master)
+
+specs2build=$(echo "${output}" | grep '^SPECS/')
+
+if [ -n "${specs2build}" ] ; then
+  # okay, we actually have something worth doing, let's get to it.
+  declare -A spec2srpm # we need this to hold the spec->srpm mappings
+  echo "CLEAN: recreating output directory ${rpmrepo}"
+  rm -rf "${rpmrepo}"
+  mkdir -p "${rpmrepo}/SRPMS"
+  echo "running builds for ${me} against the following specs: "
+  echo "${specs2build}"
+
+  # SRPMS
+  for spec in ${specs2build} ; do
+    rpmspec_out=$(./buildsystem/mksrpm.py "${spec}")
+    # this is...evil, but works Well Enough
+    rpmspec_out=$(echo "${rpmspec_out}" | sed -e 's/{//' -e 's/}//' -e "s/'//g")
+    specname=$(echo "${rpmspec_out}" | awk -F: '{ print $1 }')
+    srpmname=$(echo "${rpmspec_out}" | awk -F: '{ print $2 }')
+    spec2srpm[${specname}]="${srpmname}"
+  done
+
+  # move the SRPMS now, since that allows the job to explode later and we still get things to poke at.
+  mv SRPMS/* "${rpmrepo}/SRPMS"
+
+  # okay this one gets a little weirder - all the SRPMS are now moved, and we have a list of SPECS, but we still need to check if the supported-dists flag
+  # is about.
+  for spec in ${specs2build} ; do
+    for dist in ${DISTS} ; do
+    buildit=1 # assume we're going to do the build
+    if [ -f "SPECS/${spec}.supported-dists" ] ; then
+      grep -q "${dist}" "SPECS/${spec}.supported-dists"
+      if [ ${?} -ne 0 ] ; then
+        buildit=0	# you will not be building today
+      fi
+    fi
+    if [ ${buildit} -eq 1 ] ; then
+      # run mock, with our arg collection
+      echo mock --configdir "${MOCKCONF}" -r arrjay-${dist##el}-x86_64 -D "dist .${dist}" --rebuild "${spec2srpm[$spec]}"
+    fi
+    done
+  done
+
+  # pile any resulting dist dirs in the rpmrepo
+  for dist in ${DISTS} ; do
+    if [ -d "${dist}" ] ; then
+      mv "${dist}" "${rpmrepo}"
+    fi
+  done
+fi
